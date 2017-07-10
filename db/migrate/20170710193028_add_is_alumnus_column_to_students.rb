d2979093f82bd5e14cdd18deda6ed0e9d7f0e2cc
wratterman <will.ratterman@gmail.com>
2017-07-10 13:40:06 -0600

    student/address models created. fixed is_alumus to be default false

--- db/migrate/20170710193028_add_is_alumnus_column_to_students.rb
+++ db/migrate/20170710193028_add_is_alumnus_column_to_students.rb
@@ -1,5 +1,5 @@
 class AddIsAlumnusColumnToStudents < ActiveRecord::Migration
   def change
-    add_column :students, :is_alumnus, :boolean
+    add_column :students, :is_alumnus, :boolean, :default => false
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -32,9 +32,9 @@ ActiveRecord::Schema.define(version: 20170710193028) do
   create_table "students", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.boolean  "is_alumnus"
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
+    t.boolean  "is_alumnus", default: false
   end
 
   add_foreign_key "addresses", "students"
